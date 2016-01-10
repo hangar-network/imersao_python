7613d98e54cb5dea2b7638cf784163fdd967d11c
hallpaz <hallpaaz@gmail.com>
2016-01-10 18:00:56 -0200

    First Week updated. Working on week 2

---
+++ semana1/5 Módulos/matematica.py
@@ -0,0 +1,10 @@
+#Arquivo matematica.py
+def soma_de_quadrados(x, y):
+    res = x*x + y*y
+    return res
+
+def subtracao_de_quadrados(x, y):
+    res = x*x - y*y
+    return res
+
+variavel_teste = "Teste do módulo matematica :)"
